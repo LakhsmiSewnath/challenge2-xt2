3a3a9ff6f434cc8f5717f272d5c0f9a283654cbe
Lakhsmi Sewnath <LakshmiS@live.nl>
2019-06-20 16:33:07 +0200

    fourth commit

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@
 
 
 
-
-
 // Met deze functie wordt de tijd weergeven:
 function showTime(){
     var date = new Date();
@@ -36,32 +34,36 @@ showTime();
 
 
 
-
-
 // De is de masterTimeline. Deze zorgt ervoor dat zowel de eerste timeline als de tweede timeline worden uitgevoerd
-var masterTimeline = new TimelineMax({});
+var masterTimeline = new TimelineMax();
 masterTimeline
   .add(part1())
-  .add(part2());
+  .add(part2(),"-=10");
 
  //Dit is de timeline dat geld voor de animaties van het logo, de klok(letters) en de (laagstaande) zon of maan.
- function part1() {
+function part1() {
   var tl = new TimelineMax();
 
-  tl.fromTo(".logo", 2, {x:-300, scale:0.1}, {x:0, scale:1})
-    .fromTo(".clock", 2, {x:-300, scale:0.1}, {x:0, scale:1}, "-=2")
-    .fromTo(".moon, .sun, sun-laag", 4, {x:-10, y:0, opacity:0.0}, {ease: Back.easeOut.config(1.7), x:70, y:120 , opacity:1.0, rotation: 360}, "-=1");
+  tl.fromTo(".logo", 2, {x:-400, scale:0.1}, {x:0, rotation: 360, scale:1});
+  tl.fromTo(".clock", 2, {x:-400, scale:0.1}, {x:0, rotation: 360, scale:1},"-=2");
+  tl.fromTo(".sun", 3, {x:-400,  scale:0.1, opacity:0.0}, {x:0 , scale:1, ease: Bounce.easeOut, opacity:1});
+  tl.fromTo(".moon", 3, {x:-400,  scale:0.1}, {x:0 , scale:1, ease: Bounce.easeOut});
+  tl.fromTo(".laagstaande-sun", 3, {x:-400,  scale:0.1}, {x:0 , scale:1, ease: Bounce.easeOut});
   return tl;
 }
 
-// Dit is de timeline dat ervoor moet zorgen dat wolken continue richting de rechterkant bewegen.
+// Dit is de timeline dat ervoor moet zorgen dat wolken opkomen en weer verdwijnen.
 function part2() {
   var tl = new TimelineMax({repeat: -1, repeatDelay: 1});
 
-  tl.fromTo(".wolk1", 10, {xPercent:10, y:60, opacity:0.5}, {xPercent:100, y:60, opacity:0.0})
-     fromTo(".wolk2", 10, {xPercent:10, y:200, opacity:0.5}, {xPercent:100, opacity:0.0});
+  tl.fromTo(".wolk1", 7, {opacity:0.0}, {opacity:0.4});
+  tl.fromTo(".wolk2", 7, {opacity:0.4}, {opacity:0.0}),"-=7";
+  tl.fromTo(".wolk1", 7, {opacity:0.4}, {opacity:0.0});
+  tl.fromTo(".wolk2", 7, {opacity:0.0}, {opacity:0.4},"-=7");
+  
   return tl;
 }
+
 
 
 
